288f4e2ce266b8c75a9b447c8c31262b2fa685d1
DeathHound <acraig176@hotmail.com>
2023-05-23 14:10:49 +0100

    Partial add prerendered cutscenes (videos)

---
+++ Structures/Graphics/WIN32_CKVideo.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AliceStructures.Graphics
+{
+    [StructLayout(LayoutKind.Explicit, Size = 0x1A0, Pack = 4)]
+    public readonly unsafe struct WIN32_CKVideo
+    {
+        [FieldOffset(0xC)] private readonly string filePath;
+
+        public readonly string FilePath => filePath;
+    }
+}
