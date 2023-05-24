a561624195b77399bd2d9edf6ea874f3a88237c1
DeathHound <acraig176@hotmail.com>
2023-05-24 12:28:45 +0100

    Add other fields to Inputs

--- Structures/Managers/CKInput.cs
+++ Structures/Managers/CKInput.cs
@@ -12,7 +12,7 @@ namespace AliceStructures.Managers
         Assigning = 4,
     }
 
-    [StructLayout(LayoutKind.Explicit, Pack = 4)]
+    [StructLayout(LayoutKind.Explicit, Pack = 4, Size = 0x8C)]
     public readonly unsafe struct CKInput
     {
         [FieldOffset(0x4)] private readonly WIN32_CKPad* gamepad1;
@@ -20,8 +20,15 @@ namespace AliceStructures.Managers
         [FieldOffset(0xC)] private readonly WIN32_CKKeyboard* keyboard;
         [FieldOffset(0x1C)] private readonly InputSlot player1;
         [FieldOffset(0x20)] private readonly InputSlot player2;
+		[FieldOffset(0x2C)] private readonly CKAction* action;
+		[FieldOffset(0x38)] private readonly CKFilterBinary* binaryFilter;
+		[FieldOffset(0x40)] private readonly CKFilterAnalog* analogFilter;
+		[FieldOffset(0x44)] private readonly CKMapping* mapping;
+		[FieldOffset(0x48)] private readonly CKComboManager* comboManager;
+		[FieldOffset(0x64)] private readonly CKCombo* combo;
+		[FieldOffset(0x68)] private readonly CKComboTransition* comboTransition;
 
-        public readonly WIN32_CKPad* Gamepad1 => gamepad1;
+		public readonly WIN32_CKPad* Gamepad1 => gamepad1;
         public readonly WIN32_CKMouse* Mouse => mouse;
         public readonly WIN32_CKKeyboard* Keyboard => keyboard;
         public readonly InputSlot Player1 => player1;
