7e45863e2b94c7da5cea95a56bacda3032adbc46
DeathHound <acraig176@hotmail.com>
2023-05-25 10:34:27 +0100

    Begin adding services

---
+++ Structures/Managers/CKServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using AliceStructures.Services;
+using System.Runtime.InteropServices;
+
+namespace AliceStructures.Managers
+{
+	[StructLayout(LayoutKind.Explicit, Pack = 4)]
+	public readonly unsafe struct CKServiceManager
+	{
+		[FieldOffset(0x2C)] private readonly CKSrvCamera* camera;
+
+		public readonly CKSrvCamera* Camera => camera;
+	}
+}
